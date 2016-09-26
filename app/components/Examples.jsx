2efbcdaaf1eb3d696dcce5e865439b9b06956a9e
Ruchi <ruchigoel@Ruchis-MacBook-Air.local>
2016-09-26 15:48:55 -0700

    Add Style to weather Page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,7 @@ var {Link} = require('react-router');
 var Examples = (props) => {
   return (
     <div>
-      <h1 classNmae="text-center">Examples</h1>
+      <h1 className="text-center">Examples</h1>
       <p>Here are few examples locations to try out!</p>
           <ol>
           <li>
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -5,7 +5,7 @@ var Main = (props) => {
   return (
     <div>
       <Nav/>
-          <div className="row"> //create grid 
+          <div className="row"> 
           <div className="columns medium-6 large-4 small-centered">
                {props.children}
           </div>
