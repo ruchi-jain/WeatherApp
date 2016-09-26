977cd1d0b2bd9df971b53611954aeb4f81222217
Ruchi <ruchigoel@Ruchis-MacBook-Air.local>
2016-09-26 13:41:51 -0700

    Prep repo for Heroku

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,3 +1,4 @@
+//only operate on http, transfer all trafic all https to http
 var axios = require('axios');
 
 const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=c4e735ea8bd7e7b6dc8368c752517b2d&units=imperial';
